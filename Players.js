d22f042054de076df0e00871b64327868cd4980f
Keith K <k.kellerman@dnp.imgcomm.com>
2020-05-17 16:18:08 -0400

    Added constructors

--- Players.js
+++ Players.js
@@ -1,3 +1,17 @@
 class Player{
-    
+    //start constructors for all properties
+    constructor(name, id, color, active=false){
+        this.name = name;
+        this.id = id;
+        this.color = color;
+        this.active = active; 
+        this.tokens = [];
+
+/**
+  * Creates token objects for player
+  * @param   {integer}   num - Number of token objects to be created
+  * @return  {array}     tokens - an array of new token objects
+  */
+        createTokensnum(num)
+    }
 }
--- Token.js
+++ Token.js
@@ -1,3 +1,13 @@
 class Token{
-    
+     //start constructors for all properties
+     constructor(index, owner){
+        this.owner = owner;
+        this.id = `token-${index}-${owner.id}`; //temperate literal
+        this.dropped = false;  //default to false because at start no tokens dropped
+
+      
+
+
+    }
 }
+
