07fd2a2e0424b7adc7c64a462ac6f567fc67d834
Keith K <k.kellerman@dnp.imgcomm.com>
2020-05-17 23:15:42 -0400

    added new constructors for classes

--- Board.js
+++ Board.js
@@ -1,3 +1,29 @@
 class Board{
-    
+         //start constructors for all properties
+         constructor(){     //no arguments because board is static
+            this.row = 6;
+            this.columns=7;
+            this.spaces=this.createSpaces();
+        }
+/** 
+ * Generates 2D array of spaces. 
+ * @return  {Array}     An array of space objects
+ */
+createSpaces(){
+    const spaces = [];
+    for(let x=0; x <this.columns; x++) {
+        const column = [];
+   
+        for(let y=0; y <this.rows; y++) {
+            const space = new Space (x,y);
+            column.push(space);
+        }
+   
+        spaces.push(column);    
+        
+    }
+
+        return spaces;
+    }
+
 }
--- Players.js
+++ Players.js
@@ -5,13 +5,26 @@ class Player{
         this.id = id;
         this.color = color;
         this.active = active; 
-        this.tokens = [];
-
+        this.tokens = this.createTokens(21);   //this. is because method is available in object being init
+//21 because half of tokens on board
 /**
   * Creates token objects for player
   * @param   {integer}   num - Number of token objects to be created
   * @return  {array}     tokens - an array of new token objects
   */
-        createTokensnum(num)
+createTokensnum(num){
+    const tokens = [];
+
+    for(let i=0; i<num; i++)
+        {
+            let token= new Token(i, this);
+            tokens.push(token);
+        }
+
+      
+ return tokens;
+
+
+
     }
-}
+}}
