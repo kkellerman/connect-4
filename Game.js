ed72867e996f0cb70eac347d7e91a84705ed297c
Keith K <k.kellerman@dnp.imgcomm.com>
2020-05-23 17:36:10 -0400

    finished game class

--- Game.js
+++ Game.js
@@ -1,3 +1,23 @@
 class Game{
-    
+            //start constructors for all properties
+        constructor(){     //no arguments because board is static
+           this.board = new Board();  //sets to a new board object
+           this.players=this.createPlayers();  //hold array of two players
+           this.ready= false;
+       }
+
+       /** 
+         * Creates two player objects
+         * @return  {Array}    An array of two Player objects.
+         */
+createPlayers() {
+    const players = [new players('Player 1', 1, '#e15258',true),
+                    new players('Player 2', 2, '#e59a13')];
+
+    return players;
+
+
+}
+
+
 }
