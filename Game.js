0a5b2740475db99ed4950f18c3fab751dcce9c26
Keith K <k.kellerman@dnp.imgcomm.com>
2020-05-31 14:19:04 -0400

    finish for loop on keyhandle

--- Game.js
+++ Game.js
@@ -35,25 +35,18 @@ createPlayers() {
         * Branches code, depending on what key player presses
         * @param   {Object}    e - Keydown event object
         */
-    handleKeydown(e)
-    {  
-        if(this.ready){
-            if(e.key) ==="ArrowLeft") {
-                // move left
-            } else if ()
-
-        } else if
-
-
+       handleKeydown(e) {  
+           if(this.ready){
+               if(e.key) === "ArrowLeft") {
+                   // move left
+               } else if (e.key === "ArrowRight"){
+                   //move right
+               } else if (e.key === "ArrowDown") {
+               //play token
+   
+           } 
+      
+        }
     }
-
-
-
-    }
-
-
-
-
-
-     
 }
+
