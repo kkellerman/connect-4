23be2924c9cca8f4316724c5dfe86a4db1167115
Keith K <k.kellerman@dnp.imgcomm.com>
2020-06-14 20:53:01 -0400

    added vertical in checkForWin method

--- js/Space.js
+++ js/Space.js
@@ -33,6 +33,41 @@ get owner(){
        return this.token.owner;
    }
 
+}
+
+
+/** 
+ * Checks if there a winner on the board after each token drop.
+ * @param   {Object}    Targeted space for dropped token.
+ * @return  {boolean}   Boolean value indicating whether the game has been won (true) or not (false)
+ */
+checkForWin(target) {
+    const owner = target.token.owner;
+    let win = false
+
+     // vertical
+     for (let x = 0; x < this.board.columns; x++ ){
+        for (let y = 0; y < this.board.rows - 3; y++){
+            if (this.board.spaces[x][y].owner === owner && 
+                this.board.spaces[x][y+1].owner === owner && 
+                this.board.spaces[x][y+2].owner === owner && 
+                this.board.spaces[x][y+3].owner === owner) {
+                    win = true;
+            }           
+        }
+    }
+
+    // horizontal
+    for (let x = 0; x < this.board.columns - 3; x++ ){
+        for (let y = 0; y < this.board.rows; y++){
+            if (this.board.spaces[x][y].owner === owner && 
+                this.board.spaces[x+1][y].owner === owner && 
+                this.board.spaces[x+2][y].owner === owner && 
+                this.board.spaces[x+3][y].owner === owner) {
+                    win = true;
+            }           
+        }
+    }
 
 }
 
