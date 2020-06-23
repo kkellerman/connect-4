e12f97a9520c8ce6bb72976fc8101f69836aaeb6
kkellerman <keithrk1@gmail.com>
2020-06-22 22:51:00 -0400

    Removed redundant getter

--- js/Space.js
+++ js/Space.js
@@ -10,7 +10,7 @@ class Space{
 
 
 
-/**
+/** Getter Method
  * Checks if space has an associated token to find its owner
  * @return  {(null|Object)} Returns null or the owner object of the space's associated token.
  */
@@ -22,41 +22,22 @@ get owner() {
        }
 }
     
-
-
-
-/** Getter Method
- * Checks if space has an associated token to find its owner
- * @return  {(null|Object)} Returns null or the owner object of the space's associated token.
- */
-
+    
 /**
-	 * Checks if space has an associated token to find its owner
-	 * @return  {(null|Object)} Returns null or the owner object of the space's associated token.
-	 */
-	get owner() {
-        if (this.token === null) {
-            return null;
-        } else {
-            return this.token.owner;
-        }
-	}
-    
-    /**
-     * Draws SVG space
-     */
-	drawSVGSpace() {    
-        const svgSpace = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        svgSpace.setAttributeNS(null, "id", this.id);
-        svgSpace.setAttributeNS(null, "cx", (this.x * this.diameter) + this.radius);
-        svgSpace.setAttributeNS(null, "cy", (this.y * this.diameter) + this.radius);
-        svgSpace.setAttributeNS(null, "r", this.radius - 8);
-        svgSpace.setAttributeNS(null, "fill", "black");
-        svgSpace.setAttributeNS(null, "stroke", "none");
+* Draws SVG space
+*/
+drawSVGSpace() {    
+    const svgSpace = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    svgSpace.setAttributeNS(null, "id", this.id);
+    svgSpace.setAttributeNS(null, "cx", (this.x * this.diameter) + this.radius);
+    svgSpace.setAttributeNS(null, "cy", (this.y * this.diameter) + this.radius);
+    svgSpace.setAttributeNS(null, "r", this.radius - 8);
+    svgSpace.setAttributeNS(null, "fill", "black");
+    svgSpace.setAttributeNS(null, "stroke", "none");
 
-        document.getElementById("mask").appendChild(svgSpace);     
-    }
-    
+    document.getElementById("mask").appendChild(svgSpace);     
+}
+
     
     /**
      * Updates space to reflect a token has been dropped into it.
